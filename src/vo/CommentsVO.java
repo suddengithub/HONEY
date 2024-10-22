966ac37fd2c4d63ad452af32c2fe76288792e056
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-22 12:48:09 +0900

    간단한 VO 파일만 생성

--- src/vo/CommentsVO.java
+++ src/vo/CommentsVO.java
@@ -1,4 +1,50 @@
 package vo;
 
+import java.sql.Date;
+
 public class CommentsVO {
+    private int postNo;
+    private String nName;
+    private String content;
+    private Date cDate;
+
+    public CommentsVO(int postNo, String nName, String content) {
+        this.postNo = postNo;
+        this.nName = nName;
+        this.content = content;
+    }
+
+    public CommentsVO() {}
+
+    public int getPostNo() {
+        return postNo;
+    }
+
+    public void setPostNo(int postNo) {
+        this.postNo = postNo;
+    }
+
+    public String getnName() {
+        return nName;
+    }
+
+    public void setnName(String nName) {
+        this.nName = nName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getcDate() {
+        return cDate;
+    }
+
+    public void setcDate(Date cDate) {
+        this.cDate = cDate;
+    }
 }
--- src/vo/LikesVO.java
+++ src/vo/LikesVO.java
@@ -1,4 +1,30 @@
 package vo;
 
 public class LikesVO {
+    private int postNo;
+    private String nName;
+
+    public LikesVO(int postNo, String nName, int likes) {
+        this.postNo = postNo;
+        this.nName = nName;
+    }
+
+    public LikesVO() {}
+
+    public int getPostNo() {
+        return postNo;
+    }
+
+    public void setPostNo(int postNo) {
+        this.postNo = postNo;
+    }
+
+    public String getnName() {
+        return nName;
+    }
+
+    public void setnName(String nName) {
+        this.nName = nName;
+    }
+
 }
