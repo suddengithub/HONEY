574f8baa73e48f079b84d3000eae78baea8a1009
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-22 12:00:26 +0900

    Common paste

--- src/common/Common.java
+++ src/common/Common.java
@@ -2,6 +2,8 @@ package common;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Common {
     final static String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:xe";
@@ -18,6 +20,58 @@ public class Common {
             System.out.println(e + "연결 실패");
         }
         return conn;
+    }
+    public static void close(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e + "해제 실패");
+        }
+    }
+
+    public static void close(Statement stmt) {
+        try {
+            if(stmt != null && !stmt.isClosed()) {
+                stmt.close();
+                System.out.println("Statement 해제 성공");
+            }
+        } catch (Exception e) {
+            System.out.println(e + "해제 실패");
+        }
+    }
+
+    public static void close(ResultSet rset) {
+        try {
+            if (rset != null && !rset.isClosed()) {
+                rset.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e + "해제 실패");
+        }
+    }
+    public static void commit(Connection conn) {
+        try {
+            if(conn != null && !conn.isClosed()) {
+                conn.commit();
+                System.out.println("커밋 완료");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void rollback(Connection conn) {
+        try {
+            if(conn != null && !conn.isClosed()) {
+                conn.rollback();
+                System.out.println("롤백 완료");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
